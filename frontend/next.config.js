b8e47948cc3bc5da1cd031e8fc6be0a77c5cb987
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2023-05-17 12:29:38 -0300

    force Swc Transforms

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ['via.placeholder.com', 'plus.unsplash.com', 'images.unsplash.com'],
   },
+  experimental: {
+    forceSwcTransforms: true,
+  },
 };
 
 module.exports = nextConfig;
